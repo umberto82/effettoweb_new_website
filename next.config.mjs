be17d4420b640d358b36628023f5508c9b0c63b3
umberto82 <ufo1982u@gmail.com>
2026-07-07 11:52:09 +0200

    Ottimizzazione immagini WebP/AVIF

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/webp", "image/avif"],
+    deviceSizes: [640, 768, 1024, 1280, 1536],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
 };
 
 export default nextConfig;
